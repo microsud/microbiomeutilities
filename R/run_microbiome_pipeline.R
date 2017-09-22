117e0a3b64f728963a4de8bc4413333217da8289
Sudarshan <sudarshanshetty9@gmail.com>
2017-09-22 21:05:05 +0200

    Update run_microbiome_pipeline.R

--- R/run_microbiome_pipeline.R
+++ R/run_microbiome_pipeline.R
@@ -15,39 +15,47 @@
 #' @param filterPrev Filter OTUs not detected in less than this percent of samples
 #' @param transformation  'compositional', 'Z', 'log10', 'hellinger', 'clr' choose to tranform your counts data defaults to FALSE
 #' @param col.palette 'Spectral', 'Set2', 'Paired' use any of the RColorbrewer palette depending on number of groups you have in VaraibleA
-#'
+#' @param samsize Number of reads to rarefy your data and save the rarefied phyobject.
 #' @author Contact: SUdarshan Shetty \email{sudarshanshetty9@@gmail.com}
-#' @return Several four folders with ouput for QC, alpha diversity, ordination and others
+#' @return A \code{\link{ggplot}} plot object
 #' @export
 #' @examples \dontrun{
 #'   # Example data
-#' 
-#' run_microbiome_pipeline <- function(biom, mapping, taxonomy, treefilename,
-#'                                   out_dir, VariableA, VariableB, Distmethod, OrdinatMethod,
-#'                                   QC, filterCount, filterPrev, transformation, col.palette, filterpseq)
+#'     library(microbiome)
+#'     data(DynamicIBD)
+#'     ps0 <- DynamicIBD
+#'     p <- plot_ReadDistribution(ps0, groups='ibd_subtype', plot.type= 'density')
 #'           }
 #' @keywords utilities
 
 
 run_microbiome_pipeline <- function(biom, mapping, taxonomy, treefilename,
                                     out_dir, VariableA, VariableB, Distmethod, OrdinatMethod,
-                                    QC, filterCount, filterPrev, transformation, col.palette, filterpseq)
+                                    QC, filterCount, filterPrev, transformation, col.palette, filterpseq,samsize)
 {
-# TODO include other file formats possible with read_phyloseq function
-  ps0 <- qc_plot <- ps1 <- ps2 <- Variance.plot.a <- Variance.plot.b <- otu_tab_ps2 <- phy_tree_ps2 <- 
-  df.pd <- metadf <- metatab <- tab <- alpha_div <- alpha.plot <- ord <- NULL
-  
+
+  ps0 <- qc_plot <- ps1 <- ps2 <- Variance.plot.a <- Variance.plot.b <- otu_tab_ps2 <- phy_tree_ps2 <- df.pd <- metadf <- metatab <- tab <- alpha_div <- alpha.plot <- ord <- NULL
   oripath <- getwd()
   setwd(out_dir)
   dir.create("QC")
   dir.create("AlphaDiversity")
   dir.create("BetaDiversity")
   dir.create("Others")
+  dir.create("PhyloseqObjects")
 
   message("Building phyloseq object, may take time depending on size of the file")
   ps0 <- read_phyloseq(otu.file = biom, metadata.file = mapping, taxonomy.file = taxonomy, type = "biom")
   print(ps0);
-  treefile_p0 <- read.tree(treefilename)
+  treefile_p0 <- read.tree(treefilename);
+
+  ps0 <- merge_phyloseq(ps0, treefile_p0);
+
+  saveRDS(ps0, "./PhyloseqObjects/ps_raw.rds");
+
+  message("Below is the content of raw phyloseqobject stored as ps_raw.rds");
+  print(ps0)
+
+
   qc_plot1 <- plot_ReadDistribution(ps0, groups = VariableA,
                                     plot.type = "histogram")
   ggsave("./QC/ReadDistribution.pdf")
@@ -59,11 +67,18 @@ run_microbiome_pipeline <- function(biom, mapping, taxonomy, treefilename,
   hist(sort(sample_sums(ps0)), las = 2)
   dev.off()
 
+  barplot(sort(sample_sums(ps0)), horiz = TRUE, las = 2, sub = "Check if there is large differeence in library sizes")
+  hist(sort(sample_sums(ps0)), las = 2);
+
   message("Investigating OTU counts distribution")
   pdf("./QC/Distribution_OTU_Counts.pdf")
   barplot(sort(taxa_sums(ps0)), horiz = TRUE, las = 2, sub = "Check if there is large differeence in otu counts")
   hist(taxa_sums(ps0), las = 2, main = "raw")
-  dev.off()
+  dev.off();
+
+  barplot(sort(taxa_sums(ps0)), horiz = TRUE, las = 2, sub = "Check if there is large differeence in otu counts")
+  hist(taxa_sums(ps0), las = 2, main = "raw");
+
   ps1 <- prune_taxa(taxa_sums(ps0) > 0, ps0)
 
   message("Checking for variance in OTU counts")
@@ -71,24 +86,38 @@ run_microbiome_pipeline <- function(biom, mapping, taxonomy, treefilename,
 
   Variance.plot.a <- qplot(log10(apply(otu_table(ps1), 1, var)),
                            xlab = "log10(variance)", main = "Variance in OTUs") +
-    ggtitle("before filtering") + theme_minimal()
-  ggsave("./QC/Variance before filtering.pdf")
+    ggtitle("before filtering") + theme_minimal();
+  print(Variance.plot.a)
+  ggsave("./QC/Variance before filtering.pdf");
+
   if (filterpseq == TRUE)
   {
     message("Filtering OTUs with less than 10 counts in at least 5 % of the dataset")
     ps2 = filter_taxa(ps1, function(x) sum(x > filterCount) >
                         (filterPrev * length(x)), TRUE)
-    message("below is the infor of filtered phyobject")
+    message("Saving the filtered phyobject
+            #Saving the transformed phyloseq object as ps_filtered.rds");
+
+    saveRDS(ps2, "./PhyloseqObjects/ps_filtered.rds");
+
+    message("Below is the content of filtered phyloseqobject (based on filterCount and filterPrev) stored as ps_filtered.rds");
+
     print(ps2)
+
   } else
   {
+    message("filterpseq was false. Did not filter and hence, will not save the filtered phyloseq")
+
     ps2 <- ps1
+
   }
 
   # Filtering to remove spurious counts
   Variance.plot.b <- qplot(log10(apply(otu_table(ps1), 1, var)),
                            xlab = "log10(variance)", main = "Variance in OTUs") +
-    ggtitle("after filtering") + theme_minimal()
+    ggtitle("after filtering") + theme_minimal();
+
+  print(Variance.plot.b)
 
   ggsave("./QC/Variance After filtering.pdf")
 
@@ -113,7 +142,10 @@ run_microbiome_pipeline <- function(biom, mapping, taxonomy, treefilename,
   # All
   alpha_div <- plot_richness(ps2, color = VariableA, shape = VariableB,
                              measures = c("Observed", "Chao1", "Shannon", "InvSimpson"))
-  alpha_div <- alpha_div + geom_boxplot()
+  alpha_div <- alpha_div + geom_boxplot();
+
+  print(alpha_div);
+
   ggsave("./AlphaDiversity/Non-phylogenetic_alpha_diversity.pdf",
          height = 6, width = 18)
 
@@ -122,11 +154,22 @@ run_microbiome_pipeline <- function(biom, mapping, taxonomy, treefilename,
 
   if (transformation != FALSE)
   {
+    message("#Transformation was selected and will transform your phyloseq object
+            #Saving the transformed phyloseq object as ps_transformed.rds")
 
-    ps3 <- microbiome::transform(ps2, transform = transformation)
+    ps3 <- microbiome::transform(ps2, transform = transformation);
+
+    saveRDS(ps3, "./PhyloseqObjects/ps_transformed.rds");
+    message("Below is the content of transfored phyloseqobject (based on transformation = TRUE) stored as ps_transformed.rds");
+
+    print(ps3)
+
   } else
   {
-    ps3 <- ps2
+    ps3 <- ps2;
+    message("Below is the content of non-transfored phyloseqobject (based on transformation = FALSE)");
+
+    print(ps3)
   }
 
   ord <- ordinate(ps3, distance = Distmethod, method = OrdinatMethod)
@@ -149,8 +192,10 @@ run_microbiome_pipeline <- function(biom, mapping, taxonomy, treefilename,
   getPalette = colorRampPalette(brewer.pal(8, col.palette))  # change the palette as well as the number of colors will change according to palette.
   guide_italics <- guides(fill = guide_legend(label.theme = element_text(size = 15,
                                                                          face = "italic", colour = "Black", angle = 0)))
+
   comp.plot <- plot_composition(ps2, taxonomic.level = "Phylum",
                                 transform = "compositional")
+
   comp.plot <- comp.plot + theme_bw() +
     theme(axis.text.x = element_text(angle = 90)) +
     ggtitle("Relative abundance") + guide_italics #+
@@ -169,7 +214,34 @@ run_microbiome_pipeline <- function(biom, mapping, taxonomy, treefilename,
   }
   message("setting back to this" , setwd(oripath));
   setwd(oripath)
-  print("done!")
+
+  # Check for kurtosis
+
+  message("Using the raw phyloseq to check for kurtosis in library size")
+
+  df <- data.table(NumberReads = sample_sums(ps0), SampleID = sample_names(ps0));
+
+  require(moments);
+
+  n <- kurtosis(df$NumberReads);
+
+  if(samsize != 0){
+    ps.1 <- rarefy_even_depth(ps, sample.size = samsize)
+    saveRDS(ps.1, "./phyloseqObjects/ps_rarefyied.rds")
+  }
+
+  # check for kurtosis
+
+  if(n > 3){
+    message("Your library size is heavily tailed, considering normalising them for further analysis")
+
+  } else
+  {
+    message("Not rarefying since the variation in seq depth is not high")
+    ps1 <- ps
+  }
+  print("Done with running the command!");
+  message("Check the otuputs")
 }
 
 
@@ -214,8 +286,9 @@ plot_ReadDistribution <-
       p.dfb <-
         ggplot(df, aes(x = Reads_per_sample, fill = factor(groups))) + geom_histogram(alpha = 0.5) + facet_wrap(groups) +
         theme_minimal() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
-      print(p.dfb + ggtitle(title))
+      
     }
-
-    print("Done plotting")
+    
+    print(p.dfb + ggtitle(title))
+    
   }
